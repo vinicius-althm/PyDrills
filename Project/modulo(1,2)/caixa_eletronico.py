10cb08dfa1886f0371dbff6880ee6ce7753a3c5c
vinicius-althm <vinicius.altm@gmail.com>
2025-11-25 23:54:05 -0300

    Curso em video | Modulo 1 e 2

---
+++ Project/modulo(1,2)/caixa_eletronico.py
@@ -0,0 +1,29 @@
+texto = "Caixa eletronico"
+largura = 40
+
+print("*" * largura)  # linha superior
+print(f"*{texto:^{largura - 2}}*")  # texto centralizado com bordas laterais
+print("*" * largura)  # linha inferior
+
+
+valor_saque = int(input("Digite o valor a ser sacado: "))
+total = valor_saque
+ced = 50
+total_ced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        total_ced += 1
+
+    else:
+        if total_ced > 0:
+         print(f'Total de {total_ced} cédulas de R${ced}')
+        if ced == 50:
+             ced = 20
+        elif ced == 20:
+             ced = 10
+        elif ced == 10:
+             ced = 1
+        total_ced = 0
+        if total == 0:
+            break
