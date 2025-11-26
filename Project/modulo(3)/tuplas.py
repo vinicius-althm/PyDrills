40fa67ceeece96f807859e841ace487347bcbb7a
vinicius-althm <vinicius.altm@gmail.com>
2025-11-26 02:09:17 -0300

    Curso em video | Modulo 3

---
+++ Project/modulo(3)/tuplas.py
@@ -0,0 +1,6 @@
+texto = "TUPLAS"
+largura = 40
+
+print("*" * largura)  # linha superior
+print(f"*{texto:^{largura + 10}}*")  # texto centralizado com bordas laterais
+print("*" * largura)  # linha inferior
